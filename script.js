f1049f8e1fd25ed30c59aaf6dfb5e8db401f3a63
Lynn Kirsch <kirsch.lynn@mail.ru>
2021-06-12 12:35:38 +0700

    первый коммит

---
+++ script.js
@@ -0,0 +1,18 @@
+"use sctrict";
+
+// const result=confirm("Are you here?");
+// console.log(result);
+
+const answer=+prompt("Вам есть 18 лет?","");
+console.log(typeof(answer));
+
+
+// var name="Joe";
+// var i=0;
+// while (i<2) {
+// console.log("Happy Birtday to you!");
+// i=i+1;
+// }
+// console.log("Happy Birtday to dear, "+name+"!");
+// console.log("Happy Birtday to you!");
+
